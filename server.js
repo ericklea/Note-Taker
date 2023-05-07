29a2c7668d71e0f4b548c8201ca3939bda25f469
L E <erickson.leah.j@gmail.com>
2023-05-07 12:18:09 -0600

    re did comment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.html');
 const fs = require('fs');
-//Setting up port and requiring models for syncing
+//Setting up port
 const PORT = 3001;
 //Creating express app
 const app = express();
