25f2afa12b42dc87518faec570efa8848b4a1acf
L E <erickson.leah.j@gmail.com>
2023-05-07 12:25:21 -0600

    updated notes route

--- server.js
+++ server.js
@@ -2,14 +2,12 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.html');
-const fs = require('fs');
 //Setting up port
 const PORT = 3001;
 //Creating express app
 const app = express();
 
-app.get('/', (req, res) => res.send('Navigate to /send or/ routes'));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+//Setting up routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
