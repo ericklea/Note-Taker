623481a9cb557da7281f3c15537ea7f21824f01c
L E <erickson.leah.j@gmail.com>
2023-05-07 12:17:25 -0600

    re input port dependency

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const api = require('./routes/index.html');
 const fs = require('fs');
 //Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 //Creating express app
 const app = express();
 
