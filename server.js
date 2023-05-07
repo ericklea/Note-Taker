5cb89b4ee380596543e8402bbbffa5a3dd4e30e7
L E <erickson.leah.j@gmail.com>
2023-05-07 12:03:07 -0600

    added fs dependency

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.html');
+const fs = require('fs');
 //Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3001;
 //Creating express app
 const app = express();
+
