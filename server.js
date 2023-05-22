902a92201ea14cfd18dfa64b35b21b2bda65d616
L E <erickson.leah.j@gmail.com>
2023-05-21 18:42:59 -0600

    input get route code

--- server.js
+++ server.js
@@ -22,11 +22,20 @@ app.get('/api/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
 //POST route for notes
 app.post('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './db/db.json'));
-}
-);
+});
+
 
 //Listen to port
 app.listen(PORT, () =>
